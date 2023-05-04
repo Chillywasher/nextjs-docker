af4c0ef1aa2d84e7d9d8382fdf8c0b9ae982d361
Chillywasher <45654974+Chillywasher@users.noreply.github.com>
2023-05-04 20:13:49 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next app</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1>My Website</h1>
+      </main>
+
+      <footer className={styles.footer}>
+        &copy; Chillwasher Productions 2023
+      </footer>
+    </div>
+  )
+}
